0eda8e6b27b9f65157b0ccc3bc39188adf9f9c7f
Alex Willisson <alex.willisson@gmail.com>
2021-03-30 13:00:37 -0400

    removes unused components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,23 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatIconModule } from '@angular/material/icon';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
-import { MatDialogModule } from '@angular/material/dialog';
-import { MatInputModule } from '@angular/material/input';
-import { FormsModule } from '@angular/forms';
 
-import { ChatlogComponent } from './chatlog/chatlog.component';
-import { MessageDialogComponent } from './message-dialog/message-dialog.component';
 import { MessageComponent } from './message/message.component';
 
 import { environment } from 'src/environments/environment';
 import { AngularFireModule } from '@angular/fire';
-import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { EditorModule } from '@tinymce/tinymce-angular';
 import { MarkdownModule } from 'ngx-markdown';
 import { SecurityContext } from '@angular/core';
@@ -27,25 +18,12 @@ import { AngularSimplemdeModule } from 'angular-simplemde-resettable';
 import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ChatlogComponent,
-    MessageDialogComponent,
-    MessageComponent,
-    MessageBoxComponent,
-  ],
+  declarations: [AppComponent, MessageComponent, MessageBoxComponent],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule,
-    MatToolbarModule,
-    MatIconModule,
     MatCardModule,
     MatButtonModule,
-    MatDialogModule,
-    MatInputModule,
-    FormsModule,
     AngularFireModule.initializeApp(environment.firebase),
-    AngularFirestoreModule,
     EditorModule,
     AngularSimplemdeModule,
     MarkdownModule.forRoot({
