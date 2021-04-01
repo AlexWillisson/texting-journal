d3bddea5b51237c7907c10e9f9b36ee72c3568d0
Alex Willisson <alex.willisson@gmail.com>
2021-04-01 13:44:57 -0400

    installs better-npm-audit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,10 @@ import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatNativeDateModule } from '@angular/material/core';
 import { MatInputModule } from '@angular/material/input';
+import {
+  NgxMatDatetimePickerModule,
+  NgxMatTimepickerModule,
+} from '@angular-material-components/datetime-picker';
 
 import { MessageComponent } from './journal/message/message.component';
 import { environment } from 'src/environments/environment';
